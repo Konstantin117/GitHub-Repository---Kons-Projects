02abc2c38b474b5de11e66dd75aec8c9d4ced8d9
Konstantin <60933566+Konstantin117@users.noreply.github.com>
2020-04-14 15:02:07 -0400

    Add files via upload

---
+++ Sendgrid API Project.py
@@ -0,0 +1,17 @@
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='from_email@example.com',
+    to_emails='afaceri117@gmail.com',
+    subject='Sendgrid API Email Test',
+    html_content='<strong>Ths is my first API project using Python and Sendgrid to test automate an email!!</strong>')
+try:
+    sg = SendGridAPIClient((''))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
